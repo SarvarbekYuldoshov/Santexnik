523f2518721241499f1a2e6e3d9d58606a072735
sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2025-01-22 17:09:32 +0300

    salomlar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,7 +6,13 @@ const Header = () => {
         <div className='header'>
             <div className='header-container'>
                <ul className='header-list'>
-
+                    <li className='header-item'>
+                        <a className='header-link' href="">Ishlarimiz</a>
+                        <a className='header-link' href="">Ishlarimiz</a>
+                        <a className='header-link' href="">Ishlarimiz</a>
+                        <a className='header-link' href="">Ishlarimiz</a>
+                        <a className='header-link' href="">Ishlarimiz</a>
+                    </li>
                </ul>
             </div>
         </div>
