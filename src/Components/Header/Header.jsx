488059065e151da1fd3bbdc31c60ede923e4cb0c
sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2025-01-22 18:54:22 +0300

    salosdnds

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -9,10 +9,16 @@ const Header = () => {
             <div className='header-container'>
                <ul className='header-list'>
                     <li className='header-item'>
+                        <img className='header-img' src={Rasm_1} alt="" />
+                    </li>
+                    <li className='header-item'>
+                        <a className='header-link' href="">ASAOSIY</a>
+                    </li>
+                    <li className='header-item'>
                         <a className='header-link' href="">ISHLARIMIZ</a>
                     </li>
                     <li className='header-item'>
-                        <a className='header-link' href=""></a>
+                        <a className='header-link' href="">NIMA QILA OLAMIZ</a>
                     </li>
                     <li className='header-item'>
                         <a className='header-link' href="">BIZDA HAMMASi</a>
