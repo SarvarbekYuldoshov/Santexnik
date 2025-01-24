d0bb5b6b8b9765830ef45a95039acda4df6904a1
sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2025-01-24 19:57:34 +0300

    salomlar

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -3,7 +3,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/effect-cards';
 import { EffectCards } from 'swiper/modules';
-
+import "./Hero.css"
 export default function Hero() {
   return (
     <>
