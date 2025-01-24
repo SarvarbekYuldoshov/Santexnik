c8ae29a35e184768a8e74f0c8b0f128f417f66e5
sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2025-01-24 19:36:00 +0300

    salomlar

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,13 +1,60 @@
-import React from 'react'
+import React, { useRef, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-cards';
+import { EffectCards } from 'swiper/modules';
 
-const Hero = () => {
+export default function About() {
   return (
-    <div className='Hero'>
-       <div className='Hero-container'>
-          
-       </div>
+    <>
+    <div className='about'>
+      <div className='container about-container'>
+        <ul className='about-list'>
+          <h1 className='about-title'>Смартфоны и гаджеты для близких и друзей по доступной цене</h1>
+          <p className='about-text'>В нашем магазине вы сможете найти качествунную продукцию по досутпным ценам для вашего близкого человека</p>
+          <button className='about-btn'>Посмотреть каталог</button>
+        </ul>
+        <Swiper
+        effect={'cards'}
+        grabCursor={true}
+        modules={[EffectCards]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img className='about-img' src="https://aka-phone.netlify.app/images/smart_watch.png" alt="" />
+          <h1 className='about-title-a'>Смарт часы AmazFit</h1>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className='about-img' src="https://aka-phone.netlify.app/images/smart_watch.png" alt="" />
+          <h1 className='about-title-a'>Смарт часы AmazFit </h1>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className='about-img' src="https://aka-phone.netlify.app/images/smart_watch.png" alt="" />
+          <h1 className='about-title-a'>Смарт часы AmazFit </h1>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className='about-img' src="https://aka-phone.netlify.app/images/smart_watch.png" alt="" />
+          <h1 className='about-title-a'>Смарт часы AmazFit </h1>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className='about-img' src="https://aka-phone.netlify.app/images/smart_watch.png" alt="" />
+          <h1 className='about-title-a'>Смарт часы AmazFit </h1>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className='about-img' src="https://aka-phone.netlify.app/images/smart_watch.png" alt="" />
+          <h1 className='about-title-a'>Смарт часы AmazFit </h1>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className='about-img' src="https://aka-phone.netlify.app/images/smart_watch.png" alt="" />
+          <h1 className='about-title-a'>Смарт часы AmazFit </h1>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className='about-img' src="https://aka-phone.netlify.app/images/smart_watch.png" alt="" />
+          <h1 className='about-title-a'>Смарт часы AmazFit </h1>
+        </SwiperSlide>
+      </Swiper>
+      </div>
     </div>
-  )
+    </>
+  );
 }
-
-export default Hero
