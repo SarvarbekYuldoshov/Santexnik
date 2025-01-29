20badf27179004ab201bdfb5aead3817ed44308c
sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2025-01-29 11:18:54 +0300

    salomlar

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -52,7 +52,7 @@ export default function Hero() {
         </SwiperSlide>
         <SwiperSlide>
           <img className='hero-img' src={Rasm_9} alt="" />
-          <h1 className='about-title-a'></h1>
+          <h1 className='hero-title-a'></h1>
         </SwiperSlide>
       </Swiper>
       </div>
