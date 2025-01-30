ef89a59ac8c686bbb3092cfd4a7b5b3f6dc49c62
sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2025-01-30 20:35:29 +0300

    salomlar

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -11,6 +11,7 @@ import Rasm_8 from "../images/rasm8.jpg"
 import Rasm_9 from "../images/rasm9.jpg"
 import { EffectCards } from 'swiper/modules';
 import "./Hero.css"
+import { Button } from 'antd';
 export default function Hero() {
   return (
     <>
@@ -19,35 +20,38 @@ export default function Hero() {
          <ul className='hero-list'>
             <h1 className='hero-title'>Biz eng sungi va zamonaviy Santexnika bizning saytda</h1>
             <p className='hero-text'>Biz sizga har qanday santexnika ishlarini bajarib bera olamiz. Buzilgan joylarni ta'mirlashdan tortib, noldan qurilishgacha — ish hajmiga qarab farq qilmaydi. Barcha ishlaringizni tez va samarali amalga oshirib beramiz.</p>
-        </ul>
-        <Swiper
-        effect={'cards'}
-        grabCursor={true}
-        modules={[EffectCards]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <img className='hero-img' src={Rasm_2} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className='hero-img' src={Rasm_3} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className='hero-img' src={Rasm_4} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className='hero-img' src={Rasm_5} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className='hero-img' src={Rasm_7} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className='hero-img' src={Rasm_8} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className='hero-img' src={Rasm_9} alt="" />
-        </SwiperSlide>
-      </Swiper>
+            <Button className='hero-btn'>Ariza Qoldirish</Button>
+            </ul>
+            <ul className=''>
+                <Swiper
+                effect={'cards'}
+                grabCursor={true}
+                modules={[EffectCards]}
+                className="mySwiper"
+              >
+                <SwiperSlide>
+                  <img className='hero-img' src={Rasm_2} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img className='hero-img' src={Rasm_3} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img className='hero-img' src={Rasm_4} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img className='hero-img' src={Rasm_5} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img className='hero-img' src={Rasm_7} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img className='hero-img' src={Rasm_8} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img className='hero-img' src={Rasm_9} alt="" />
+                </SwiperSlide>
+              </Swiper>
+          </ul>
       </div>
     </div>
     </>
